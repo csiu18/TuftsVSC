808c9feb628db2187e04ccecf955648e1cd8f26f
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-06-13 20:42:00 -0400

    First draft of e-board section

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+// set profile pictures
+window.onload = function () {
+	const pictures = [
+		'jen.png',
+		'tu-anh.png',
+		'daniel.png',
+		'huyen.png',
+		'ricki.png',
+		'cindy.png',
+		'britnie.png',
+	];
+	const offset = '../assets/profiles/';
+	const profileElems = document.querySelectorAll('.profile');
+
+	for (let i = 0; i < pictures.length; i++) {
+		profileElems[i].style.background =
+			'url(' + offset + pictures[i] + ') center';
+		profileElems[i].style.backgroundSize = 'cover';
+	}
+};
