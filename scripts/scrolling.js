d83b9c24f7ec680b49466e54f05b7d5f75a0aa2e
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-06-24 17:38:18 -0400

    modified scrolling for new gallery page

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,23 +1,33 @@
 // SOURCE: w3schools
 $(document).ready(function () {
+	// Check if 'scroll' cookies exists
+	if (Cookies.get('scroll')) {
+		scrollToHash(Cookies.get('scroll'));
+		Cookies.remove('scroll');
+	}
 	// Add smooth scrolling to all links
 	$('a').on('click', function (event) {
 		// Make sure this.hash has a value before overriding default behavior
 		if (this.hash !== '') {
 			// Prevent default anchor click behavior
 			event.preventDefault();
-
-			// Store hash
-			var hash = this.hash;
-
-			// Using jQuery's animate() method to add smooth page scroll
-			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-			$('html, body').animate(
-				{
-					scrollTop: $(hash).offset().top - $('nav').height(),
-				},
-				800
-			);
+			// If on gallery, refresh to homepage
+			if (window.location.pathname === '/gallery.html') {
+				Cookies.set('scroll', this.hash);
+				window.location.replace('/');
+			}
+			scrollToHash(this.hash);
 		} // End if
 	});
 });
+
+function scrollToHash(hash) {
+	// Using jQuery's animate() method to add smooth page scroll
+	// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	$('html, body').animate(
+		{
+			scrollTop: $(hash).offset().top - $('nav').height(),
+		},
+		800
+	);
+}
