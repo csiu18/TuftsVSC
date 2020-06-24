232b7e2345754ad81c184a7c00b880326e703a76
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-06-24 00:55:06 -0400

    First draft of events section

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-// set profile pictures
 window.onload = function () {
+	// set profile pictures
 	const pictures = [
 		'jen.png',
 		'tu-anh.png',
@@ -17,4 +17,9 @@ window.onload = function () {
 			'url(' + offset + pictures[i] + ') center';
 		profileElems[i].style.backgroundSize = 'cover';
 	}
+
+	// set video height
+	const video = document.querySelector('#spring > div:last-of-type > iframe');
+	const picture = document.querySelector('#spring > div:last-of-type > img');
+	video.style.height = picture.style.height;
 };
