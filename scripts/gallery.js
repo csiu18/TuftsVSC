ec94901e36a12845807017c04a7ca0ef40e1c41e
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-06-26 22:12:05 -0400

    fixed gallery animation for mobile

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,38 +3,57 @@ const IMGS_PER_COL = 9;
 const PATH = 'assets/gallery/';
 const IMG_TYPE = '.jpg';
 
-/* global IntersectionObserver */
-let sentinels = document.querySelectorAll('.sentinel');
-let indices = {
-	section0: 0,
-	section1: 0,
-	section2: 0,
+window.onload = function () {
+	if (matchMediaQuery('(max-width: 767.98px)')) loadForMobile();
+	else loadForDesktop();
 };
 
-function loadImage(section) {
+function loadImage(section, offset, newClass = undefined) {
 	let id = parseInt(section.id.slice(-1));
-	if (
-		id !== 0 &&
-		matchMediaQuery('(max-width: 767.98px)') &&
-		indices[`section${id - 1}`] < IMGS_PER_COL
-	)
-		return;
 	let image = document.createElement('img');
-	let imageID = id * 9 + indices[section.id]++;
+	let imageID = id * 9 + offset;
 	image.src = PATH + imageID + IMG_TYPE;
 	image.alt = `gallery photo ${imageID}`;
 	section.appendChild(image);
-	image.className = 'scale-in-center';
+	if (newClass) image.className = newClass;
 }
 
-let intersectionObserver = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		let section = entry.target.parentElement;
-		if (entry.intersectionRatio > 0 && indices[section.id] < IMGS_PER_COL) {
-			loadImage(section);
-			section.appendChild(entry.target);
+function loadForMobile() {
+	let sections = document.querySelectorAll('.section');
+	for (let section of sections) {
+		for (let i = 0; i < IMGS_PER_COL; i++) {
+			loadImage(section, i);
 		}
+	}
+	let images = document.querySelectorAll('.section img');
+	let intersectionObserver = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.intersectionRatio > 0) {
+				entry.target.className = 'scale-in-center';
+			}
+		});
 	});
-});
+	for (let image of images) intersectionObserver.observe(image);
+}
 
-for (let sentinel of sentinels) intersectionObserver.observe(sentinel);
+function loadForDesktop() {
+	let sentinels = document.querySelectorAll('.sentinel');
+	let indices = {
+		section0: 0,
+		section1: 0,
+		section2: 0,
+	};
+	let intersectionObserver = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			let section = entry.target.parentElement;
+			if (
+				entry.intersectionRatio > 0 &&
+				indices[section.id] < IMGS_PER_COL
+			) {
+				loadImage(section, indices[section.id]++, 'scale-in-center');
+				section.appendChild(entry.target);
+			}
+		});
+	});
+	for (let sentinel of sentinels) intersectionObserver.observe(sentinel);
+}
