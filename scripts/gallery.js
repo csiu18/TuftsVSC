032672f2ac053a49135d8239b2576f1b9673ee07
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-06-26 22:31:10 -0400

    removed infinite scrolling

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -4,21 +4,6 @@ const PATH = 'assets/gallery/';
 const IMG_TYPE = '.jpg';
 
 window.onload = function () {
-	if (matchMediaQuery('(max-width: 767.98px)')) loadForMobile();
-	else loadForDesktop();
-};
-
-function loadImage(section, offset, newClass = undefined) {
-	let id = parseInt(section.id.slice(-1));
-	let image = document.createElement('img');
-	let imageID = id * 9 + offset;
-	image.src = PATH + imageID + IMG_TYPE;
-	image.alt = `gallery photo ${imageID}`;
-	section.appendChild(image);
-	if (newClass) image.className = newClass;
-}
-
-function loadForMobile() {
 	let sections = document.querySelectorAll('.section');
 	for (let section of sections) {
 		for (let i = 0; i < IMGS_PER_COL; i++) {
@@ -34,26 +19,13 @@ function loadForMobile() {
 		});
 	});
 	for (let image of images) intersectionObserver.observe(image);
-}
+};
 
-function loadForDesktop() {
-	let sentinels = document.querySelectorAll('.sentinel');
-	let indices = {
-		section0: 0,
-		section1: 0,
-		section2: 0,
-	};
-	let intersectionObserver = new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-			let section = entry.target.parentElement;
-			if (
-				entry.intersectionRatio > 0 &&
-				indices[section.id] < IMGS_PER_COL
-			) {
-				loadImage(section, indices[section.id]++, 'scale-in-center');
-				section.appendChild(entry.target);
-			}
-		});
-	});
-	for (let sentinel of sentinels) intersectionObserver.observe(sentinel);
+function loadImage(section, offset) {
+	let id = parseInt(section.id.slice(-1));
+	let image = document.createElement('img');
+	let imageID = id * 9 + offset;
+	image.src = PATH + imageID + IMG_TYPE;
+	image.alt = `gallery photo ${imageID}`;
+	section.appendChild(image);
 }
