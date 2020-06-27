3075eaaf340ca1865cffcc6177a109727eac4feb
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-06-27 18:04:57 -0400

    fixed animation for photo gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,9 +1,12 @@
 const NUM_IMAGES = 27;
-const IMGS_PER_COL = 9;
+const NUM_COL = 3;
+const IMGS_PER_COL = NUM_IMAGES / NUM_COL;
 const PATH = 'assets/gallery/';
 const IMG_TYPE = '.jpg';
 const imageURLS = new Array();
 const images = new Array();
+const reset = new Array();
+let fullReset = false;
 
 window.onload = function () {
 	// preload images
@@ -11,17 +14,34 @@ window.onload = function () {
 		imageURLS[i] = PATH + i + IMG_TYPE;
 	}
 	preload(...imageURLS);
-
+	let options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 1.0,
+	};
 	// add animation to images
 	let images = document.querySelectorAll('.section img');
 	let intersectionObserver = new IntersectionObserver((entries) => {
 		entries.forEach((entry) => {
-			if (entry.intersectionRatio > 0) {
+			if (entry.isIntersecting) {
 				entry.target.className = 'scale-in-center';
+			} else {
+				if (!reset[entry.target.id]) {
+					reset[entry.target.id] = true;
+					entry.target.classList.remove('scale-in-center');
+				}
 			}
 		});
 	});
-	for (let image of images) intersectionObserver.observe(image);
+	for (let image of images) intersectionObserver.observe(image, options);
+};
+
+window.onscroll = function () {
+	if (!fullReset) {
+		console.log(reset);
+		for (let i = 0; i < reset.length; i++) reset[i] = true;
+		fullReset = true;
+	}
 };
 
 function preload(...urls) {
@@ -30,6 +50,8 @@ function preload(...urls) {
 	for (let i = 0; i < urls.length; i++) {
 		images[i] = new Image();
 		images[i].src = urls[i];
+		images[i].id = i;
+		reset[i] = false;
 		if (i !== 0 && i % IMGS_PER_COL === 0) sectionID++;
 		sections[sectionID].append(images[i]);
 	}
